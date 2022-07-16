6e59ee91d1f7a651526b6731a49fb93def23cc11
devdan <oluochdancun19@gmail.com>
2022-07-16 14:34:32 +0300

    mod

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      {props.bio && props.bio>0? <p>{props.bio}</p> : null}
+      {props.bio && props.bio.length >0? <p>{props.bio}</p> : null}
       
       {/* <p>{props.bio}</p> */}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
